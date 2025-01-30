7557c93dbd09232690c0ba140f7afdf72ed7b88a
CARDONA LUIS <kikecar97@gmail.com>
2025-01-30 14:48:12 -0600

    actualizar hora

--- src/app/pages/responder-solicitudes-agente-seguridad/responder-solicitudes-agente-seguridad.component.ts
+++ src/app/pages/responder-solicitudes-agente-seguridad/responder-solicitudes-agente-seguridad.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { Console } from 'node:console';
 
 @Component({
   selector: 'app-responder-solicitudes-agente-seguridad',
@@ -16,19 +17,21 @@ export class ResponderSolicitudesAgenteSeguridadComponent implements OnInit{
   solicitudes: any[] = [];
   apiUrl = `${environment.apiUrl}/aprobarSolicitudesAgente/`;
 
-
   empleado = {
     fecha: '',
     tipoSolicitud: '',
     nombre: '',
-    estadoSolicitud: ''
+    estadoSolicitud: '',
+    horaRetorno:''
   };
 
   modalTitle: string = '';
   modalMessage: string = '';
   horaEntrada: string = '';
   horaSalida: string = '';
+  horaRetorno: string = '';
   @ViewChild('modalContent') modalContent: any;
+  modalRef: any;
 
   constructor(private http: HttpClient, private modalService: NgbModal) {}
   
@@ -39,7 +42,8 @@ export class ResponderSolicitudesAgenteSeguridadComponent implements OnInit{
     day: '2-digit'
   }).split('/').reverse().join('-');
 
-  solicitudSeleccionada: any = null;
+  solicitudSeleccionada: any = {};
+
   mot_rechazo: string = '';
 
   ngOnInit() {
@@ -50,6 +54,7 @@ export class ResponderSolicitudesAgenteSeguridadComponent implements OnInit{
     this.http.get<any[]>(this.apiUrl).subscribe({
       next: (data) => {
         this.solicitudes = data;
+        console.log(this.solicitudes);
       },
       error: (error) => {
       }
@@ -58,6 +63,13 @@ export class ResponderSolicitudesAgenteSeguridadComponent implements OnInit{
   openModal(solicitud: any) {
     this.solicitudSeleccionada = solicitud;
     this.modalTitle = solicitud.nom_tipo_solicitud;
+    
+    // Validar estado de hora salida
+    this.solicitudSeleccionada.horaSalidaBloqueada = solicitud.hor_salida !== null;
+    
+    // Validar si se puede mostrar hora retorno
+    this.solicitudSeleccionada.horaRetornoBloqueada = !solicitud.hor_salida;
+    
     const modalRef = this.modalService.open(this.modalContent, {
       centered: true,
       backdrop: 'static',
@@ -65,8 +77,67 @@ export class ResponderSolicitudesAgenteSeguridadComponent implements OnInit{
     });
   }
 
-  aprobarSolicitud() {
-     
+  abrirModal(content: any, solicitud: any) {
+    this.solicitudSeleccionada = solicitud;
+    this.solicitudSeleccionada.hor_salida_registrada = solicitud.hor_salida !== null;
+    this.horaRetorno = solicitud.hor_retorno || '';
+    this.modalRef = this.modalService.open(content, { size: 'lg' });
+  }
+
+  enviarHoraSalida() {
+    const token = localStorage.getItem('token');
+    let userEmail = '';
+    
+    if (token) {
+      const tokenData = JSON.parse(atob(token.split('.')[1]));
+      userEmail = tokenData.sub;
+    }
+
+    const payload = {
+      id_permiso: this.solicitudSeleccionada.id_permiso,
+      tipo_permiso: this.solicitudSeleccionada.nom_tipo_solicitud,
+      agente_aprobacion: userEmail,
+      hor_salida: this.solicitudSeleccionada.hor_salida
+    };
+    
+    this.http.put(this.apiUrl, payload).subscribe({
+      next: (response) => {
+        this.solicitudSeleccionada.hor_salida_registrada = true;
+        this.obtenerTodasLasSolicitudes();
+        this.modalService.dismissAll();
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      }
+    });
+  }
+
+  enviarHoraRetorno() {
+    const token = localStorage.getItem('token');
+    let userEmail = '';
+    
+    if (token) {
+      const tokenData = JSON.parse(atob(token.split('.')[1]));
+      userEmail = tokenData.sub;
+    }
+
+    const payload = {
+      id_permiso: this.solicitudSeleccionada.id_permiso,
+      tipo_permiso: this.solicitudSeleccionada.nom_tipo_solicitud,
+      agente_aprobacion: userEmail,
+      hor_retorno: this.horaRetorno
+    };
+    
+    // TODO: Agregar endpoint para hora retorno
+    this.http.put(`${this.apiUrl}/retorno`, payload).subscribe({
+      next: (response) => {
+        this.obtenerTodasLasSolicitudes();
+        this.modalService.dismissAll();
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      }
+    });
   }
 
   limpiarFormulario() {
