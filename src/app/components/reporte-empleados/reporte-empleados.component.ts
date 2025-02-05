0eb73753831aafceeaaf80ad4f7168676aeee960
CARDONA LUIS <kikecar97@gmail.com>
2025-02-05 15:52:30 -0600

    update reporte empleados

--- src/app/components/reporte-empleados/reporte-empleados.component.ts
+++ src/app/components/reporte-empleados/reporte-empleados.component.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-reporte-empleados',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reporte-empleados.component.html',
   styleUrl: './reporte-empleados.component.css'
 })
-export class ReporteEmpleadosComponent {
+export class ReporteEmpleadosComponent implements OnInit {
+  reportes: any[] = [];
+  apiUrl = `${environment.apiUrl}/reportePermisos`;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.obtenerReporte();
+  }
+
+  obtenerReporte() {
+    this.http.get<any[]>(this.apiUrl).subscribe({
+      next: (data) => {
+        let currentDep = '';
+        let currentEmp = '';
+        let rowspanDep = 1;
+        let rowspanEmp = 1;
+        let employeeCount = 0;
+        
+        this.reportes = data.map((item, index) => {
+          if (index > 0) {
+            if (item.nom_dependencia === data[index - 1].nom_dependencia) {
+              item.showDep = false;
+              data[index - 1].rowspanDep = rowspanDep += 1;
+            } else {
+              rowspanDep = 1;
+              item.showDep = true;
+            }
+            
+            if (item.empleado === data[index - 1].empleado) {
+              item.showEmp = false;
+              data[index - 1].rowspanEmp = rowspanEmp += 1;
+              item.empleadoClass = data[index - 1].empleadoClass;
+            } else {
+              rowspanEmp = 1;
+              item.showEmp = true;
+              employeeCount++;
+              item.empleadoClass = employeeCount % 2 === 0 ? 'empleado-par' : 'empleado-impar';
+            }
+          } else {
+            item.showDep = true;
+            item.showEmp = true;
+            item.empleadoClass = 'empleado-impar';
+          }
+          return item;
+        });
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      }
+    });
+  }
 }
