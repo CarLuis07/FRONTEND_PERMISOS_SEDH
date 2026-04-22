795f1011d195d6910b269a0e52f274cf976df5da
SEDH <infotecnologia@sedh.gob.hn>
2026-04-22 16:53:27 -0600

    cambio

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './services/httpInterceptor';
@@ -12,9 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
-    { provide: FormsModule, useValue: FormsModule },
-    { provide: RouterModule, useValue: RouterModule }
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { authInterceptor } from './app/services/httpInterceptor';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(
-      withFetch(),
-      withInterceptors([authInterceptor])
-    ),
-    provideRouter(routes)
-  ],
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
